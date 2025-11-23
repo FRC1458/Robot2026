184e6425e75b405b47aa0ddb300a65bbc54032b6
akhil.space <145620839+aerospikeengineer@users.noreply.github.com>
2025-11-23 13:47:13 -0800

    thing

--- src/main/java/frc/robot/subsystems/algaearm/AlgaeArm.java
+++ src/main/java/frc/robot/subsystems/algaearm/AlgaeArm.java
@@ -2,10 +2,9 @@ package frc.robot.subsystems.algaearm;
 
 import com.ctre.phoenix6.controls.ControlRequest;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.math.MathUtil;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.util.sendable.SendableBuilder;
@@ -18,15 +17,13 @@ public class AlgaeArm extends SubsystemBase {
     private ControlRequest request;
     private final TalonFX pivotMotor;
     private double currentAngle;
-    private double currentVelocity;
-	
+
     public static AlgaeArm getInstance() {
         if (algaeArmInstance == null) {
             algaeArmInstance = new AlgaeArm();
         }
         return algaeArmInstance;
     }
-
 
     private AlgaeArm() {
         pivotMotor = new TalonFX(AlgaeArmConstants.Motors.PIVOT_MOTOR.id);
@@ -38,51 +35,31 @@ public class AlgaeArm extends SubsystemBase {
 
     @Override
     public void periodic() {
-
         currentAngle = AlgaeArmConstants.rotationsToAngle(pivotMotor.getPosition().getValueAsDouble());
-        
         pivotMotor.setControl(request);
-        
-        currentVelocity = pivotMotor.getVelocity().getValueAsDouble();
     }
-
 
     public Command moveToAngle(double targetAngle) {
         return defer(() -> {
-
-            double targetRotations = AlgaeArmConstants.angleToRotations(
-                MathUtil.clamp(targetAngle, AlgaeArmConstants.MIN_ANGLE, AlgaeArmConstants.MAX_ANGLE));
-
-            return runOnce(() -> setRequest(new MotionMagicVoltage(targetRotations)))
+            double targetRotations = AlgaeArmConstants.angleToRotations(targetAngle);
+			
+            return runOnce(() -> setRequest(new MotionMagicVoltage(targetRotations))) // Move motor to target
                 .withName("Moving to angle: " + targetAngle);
         });
-    }
-
-
-    public Command stop() {
-        return runOnce(() -> setRequest(
-            new PositionVoltage(pivotMotor.getPosition().getValue())))
-            .withName("Stop Arm");
-    }
-	
-    public double getCurrentAngle() {
-        return currentAngle;
-    }
-
-    public double getCurrentVelocity() {
-        return currentVelocity;
     }
 
     @Override
     public void initSendable(SendableBuilder builder) {
         super.initSendable(builder);
-        
         builder.addDoubleProperty("Arm Angle", this::getCurrentAngle, null);
-        builder.addDoubleProperty("Arm Velocity (RPM)", this::getCurrentVelocity, null);
         
         TelemetryManager.makeSendableTalonFX("Pivot Motor", pivotMotor, builder);
     }
 
+    public double getCurrentAngle() {
+        return currentAngle;
+    }
+	
     private void setRequest(ControlRequest request) {
         this.request = request;
     }
