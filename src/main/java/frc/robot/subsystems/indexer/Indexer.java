204c13c72b2a0a43fb7e5938a47419996f57b472
Ethan Lian <ethanlian2000@gmail.com>
2026-01-14 17:25:09 -0800

    dsljfs

---
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.indexer;
+import static frc.robot.subsystems.indexer.IndexerConstants;
+
+public class Indexer {
+    /* forever loop that constantly  
+     * 1) finds shooterMax
+     * 2) finds shooterFuel
+     * 3) calculates shooterSpace
+     * 4) Based on shooter
+     */
+}
---
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -0,0 +1,6 @@
+/* intake -> storage -> indexer */
+public class IndexerConstants {
+    int shooterFuel = 3; /* something that detects the # of fuel in shooter */
+    int shooterMax = 5; /* max capacity of shooter */
+    int shooterSpace = 0; /* space left */
+}
