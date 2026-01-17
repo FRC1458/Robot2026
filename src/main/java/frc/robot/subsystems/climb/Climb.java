f867e8e067fc42e0dc0ae4dc50db4c2cad7045ac
shakability <armaangulzar@gmail.com>
2026-01-17 13:48:06 -0800

    i think im done but you're gonna need the motor id as well as to tune all the stuff marked in climbconstants

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -1,5 +1,176 @@
 package frc.robot.subsystems.climb;
 
-public class Climb {
-    
+import static frc.robot.subsystems.climb.ClimbConstants.*;
+
+import com.ctre.phoenix6.SignalLogger;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.lib.util.Util;
+import frc.robot.subsystems.TelemetryManager;
+
+public class Climb extends SubsystemBase {
+	private static Climb climbInstance;
+	public static Climb getInstance() {
+		if (climbInstance == null) {
+			climbInstance = new Climb();
+		}
+		return climbInstance;
+	}
+
+	private final TalonFX climbMotor;
+
+	private double lastReadHeight;
+	private double lastReadSpeed;
+
+	private double targetHeight = 0;
+	private ControlRequest request = new NeutralOut();
+
+	private Climb() {
+		super();
+		climbMotor = new TalonFX(CLIMB_MOTOR_ID);
+		climbMotor.getConfigurator().apply(getConfig());
+		climbMotor.setNeutralMode(NeutralModeValue.Brake);
+		TelemetryManager.getInstance().addSendable(this);
+		setDefaultCommand(stop());
+	}
+
+	@Override
+	public void periodic() {
+		// Read the height from the motor encoder
+		lastReadHeight = rotationsToMeters(
+			climbMotor.getPosition().getValueAsDouble());
+		lastReadSpeed = rotationsToMeters(
+			climbMotor.getVelocity().getValueAsDouble());
+		// updates the motor
+		climbMotor.setControl(request);
+	}
+
+	/** Swaps the control request */
+	private void setRequest(ControlRequest request) {
+		this.request = request;
+	}
+
+	private Command moveToScoringHeight(Heights height) {
+		return moveToTarget(height.height).withName(height.name() + ": Unsafe, Moving");
+	}
+
+	/** Attempts to move the end effector to a height, in meters */
+	private Command moveToTarget(double targetHeight) {
+		return runOnce(
+			() -> {
+				this.targetHeight = targetHeight;
+				setRequest(new MotionMagicVoltage(
+					metersToRotations(
+						targetHeight)));
+			}
+		).andThen(
+			Commands.waitUntil(() -> isNearTarget())
+		).withName(String.format("%.2f: Unknown, Moving", targetHeight));
+	}
+
+	/** Stops the elevator */
+	public Command stop() {
+		return runOnce(
+			() -> setRequest(
+				new PositionVoltage(
+					metersToRotations(targetHeight))))
+			.withName("Stopped");
+	}
+
+	/** Sysid commands
+	 * @param dynamic If true, then runs dynamic test. If false, quasistatic
+	 */
+	public Command sysId(boolean dynamic, SysIdRoutine.Direction direction) {
+		return defer(() -> {
+			VoltageOut request = new VoltageOut(0);
+			SysIdRoutine sysIdRoutine = new SysIdRoutine(
+				new SysIdRoutine.Config(
+					null, // Default ramp rate (1 V)
+					null, // Default step voltage (7 V)
+					null, // Use default timeout (10 s)
+					// Log state with SignalLogger class
+					state -> SignalLogger.writeString("SysIdElevator_State", state.toString())
+				),
+				new SysIdRoutine.Mechanism(
+					output -> request.withOutput(output),
+					null,
+					this
+				)
+			);
+			if (dynamic) {
+				return sysIdRoutine.dynamic(direction);
+			} else {
+				return sysIdRoutine.quasistatic(direction);
+			}
+		});
+	}
+
+	public double estimateTimeToTarget(Heights height) {
+		return estimateTimeToTarget(height.height);
+	}
+
+	public double estimateTimeToTarget(double targetHeight) {
+		return Util.trapezoidProfileTimeToTarget(
+			lastReadHeight, lastReadSpeed, 
+			targetHeight, MAX_SPEED, MAX_ACCEL) 
+			+ 0.3; // Jerk offset
+	}
+
+	public double estimateTimeRemaining() {
+		return Util.trapezoidProfileTimeToTarget(
+			lastReadHeight, lastReadSpeed, targetHeight, 
+			MAX_SPEED, MAX_ACCEL) 
+			+ 0.16; // Jerk offset
+	}
+
+	/** Checks if the end effector is within 1 cm of the target */
+	public boolean isNearTarget() {
+		return MathUtil.isNear(
+			lastReadHeight,
+			targetHeight,
+			EPSILON);
+	}
+
+	@Override
+	public void initSendable(SendableBuilder builder) {
+		super.initSendable(builder);
+		builder.addDoubleProperty(
+			"Height",
+			() -> lastReadHeight,
+			null);
+		builder.addDoubleProperty(
+			"Speed",
+			() -> lastReadSpeed,
+			null);
+		builder.addDoubleProperty(
+			"Target Height",
+			() -> targetHeight,
+			null);
+		TelemetryManager.makeSendableTalonFX("ClimbMotor", climbMotor, builder);
+	}
 }
---
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -0,0 +1,69 @@
+package frc.robot.subsystems.climb;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.VoltageConfigs;
+
+import edu.wpi.first.math.util.Units;
+import frc.robot.Constants;
+
+public class ClimbConstants {
+
+    public static final double EPSILON = 0.03; // Meters
+
+    public static final double SPROCKET_RADIUS = 0.0412; // Effective pitch radius
+    public static final double GEAR_RATIO = 9;
+    public static final double SPROCKET_CIRCUMFERENCE = SPROCKET_RADIUS * Constants.TAU;
+    public static final double METERS_PER_ROTATION = 0.028776; // Approximated using measurement
+    public static final double CARRIAGE_WEIGHT = 7.55; // kg
+
+    // Yo uu hav eto tTune all of this above me
+
+    public static final double MAX_ACCEL = 1.5;
+    public static final double MAX_SPEED = 1.0; // m/s
+    
+    public static final int CLIMB_MOTOR_ID = 67; // change this
+
+    public static enum Heights {
+        BASE(0.003), // small offset to prevent stalling (allegedly)
+        UP(Units.inchesToMeters(27.0));
+        public final double height;
+        private Heights(double height) {
+            this.height = height;
+        }
+    }
+
+    public static TalonFXConfiguration getConfig() {
+        return new TalonFXConfiguration()
+            .withSlot0(new Slot0Configs()
+                .withKS(0.125)
+                .withKV(0.0)
+                .withKP(1.0)
+                .withKI(0.0)
+                .withKD(0.05)
+                .withKG(0.375))
+            .withMotionMagic(new MotionMagicConfigs()
+                .withMotionMagicAcceleration(metersToRotations(MAX_ACCEL)) // 1.0 m/s^2
+                .withMotionMagicCruiseVelocity(metersToRotations(MAX_SPEED))
+                .withMotionMagicJerk(320))
+            .withCurrentLimits(new CurrentLimitsConfigs()
+                .withStatorCurrentLimit(60)
+                .withSupplyCurrentLimit(60))
+            .withVoltage(new VoltageConfigs()
+                .withPeakForwardVoltage(12.0)
+                .withPeakReverseVoltage(-12.0));
+    }
+
+    /** Conversion utility */
+    public static double rotationsToMeters(double rotations) {
+        return rotations * METERS_PER_ROTATION;
+    }
+    
+    /** Conversion utility */
+    public static double metersToRotations(double meters) {
+        return meters / METERS_PER_ROTATION;
+    }
+
+}
