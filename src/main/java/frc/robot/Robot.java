8be22239b5ba9a47b0489f2a7488491720fc00f4
rriofrio24 <riofrio.rafael@outlook.com>
2026-01-21 15:30:11 -0800

    I added AdvantageKit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,13 @@ import frc.robot.auto.AutoSelector;
 
 import java.util.Optional;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import com.pathplanner.lib.commands.FollowPathCommand;
 
 import edu.wpi.first.epilogue.Logged;
@@ -29,7 +36,7 @@ import frc.robot.subsystems.vision.VisionDeviceManager;
  * this project, you must also update the Main.java file in the project.
  */
 @SuppressWarnings("unused")
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
 	private static final CommandScheduler commandScheduler = CommandScheduler.getInstance();
 	private AutoSelector autoChooser;
 	private Command autoCommand;
@@ -42,6 +49,7 @@ public class Robot extends TimedRobot {
 	 * initialization code.
 	 */
 	public Robot() {
+		
 		if (Robot.isReal()) {
 			VisionDeviceManager.getInstance();
 		}
@@ -60,6 +68,21 @@ public class Robot extends TimedRobot {
 		  System.out.println("Log/USB mounts NOT OK");
 		}
 		DriverStation.startDataLog(DataLogManager.getLog());
+		Logger.recordMetadata("ProjectName", "MyProject"); // Set a metadata value
+
+		if (isReal()) {
+    		Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+    		Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+		} else {
+    		setUseTiming(false); // Run as fast as possible
+    		String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+    		Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+    		Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+		}
+		Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+		// ...
+		Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+
 	}
 
 	/**
