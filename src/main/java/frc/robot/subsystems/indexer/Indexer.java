46bb9fdba3dfe89b4a1d276d8f14f9bd3061e6db
Ethan Lian <ethanlian2000@gmail.com>
2026-01-20 17:30:59 -0800

    L rizz fixes

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -1,67 +1,81 @@
 package frc.robot.subsystems.indexer;
-import static frc.robot.subsystems.indexer.IndexerConstants.*;
-
-import java.util.ResourceBundle.Control;
-
 import com.ctre.phoenix6.controls.ControlRequest;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import au.grapplerobotics.LaserCan;
+import au.grapplerobotics.ConfigurationFailedException;
 
-public class Indexer extends SubsystemBase{
+public class Indexer extends SubsystemBase {
     private ControlRequest request;
 
     private TalonFX motor;
-    private SomeBrand laser; // fix later
+    private LaserCan lc;
 
-    private boolean ballz = false;
+    private double getDistanceMm() {
+        LaserCan.Measurement measurement = lc.getMeasurement();
+        if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+            return measurement.distance_mm;
+        } else {
+            return -1;
+        }
+    }
+
+    private boolean hasBall; 
+
+    private boolean hasBall() {
+        return hasBall;
+    }
+
+    private void checkForBall() {
+        double x = getDistanceMm();
+        if (x == IndexerConstants.LaserCan_DefaultMeasurement) {
+            boolean hasBall = false;
+        } else {
+            boolean hasBall = true;
+        }
+    }
 
     private Indexer() {
         motor = new TalonFX(IndexerConstants.MOTOR_ID);
-    }
 
-    private void IndexerSensor() {
-        laser = new SomeBrand(IndexerConstants.LASER_ID);
+        lc = new LaserCan(IndexerConstants.LASER_ID);
+        try {
+            lc.setRangingMode(LaserCan.RangingMode.SHORT);
+            lc.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+            lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+          } catch (ConfigurationFailedException e) {
+            System.out.println("Configuration failed! " + e);
+          }
     }
 
     @Override
     public void periodic() {
         motor.setControl(request);
-        laser.setControl(request);
-    }
+        checkForBall();
+    };
 
     public void setRequest(ControlRequest request) {
         this.request = request;
-    }
+    };
 
-    public Command setSpeed(double speed) {
+    public  Command setSpeed(double speed) {
         return runOnce(() -> setRequest(new VelocityVoltage(speed)));
-    }
+    };
     
     public Command activateIndexer() {
-        return setSpeed(IndexerConstants.ActiveSpeed);
+        return Commands.either (
+            setSpeed(IndexerConstants.ActiveSpeed),
+            setSpeed(IndexerConstants.InactiveSpeed),
+            this::hasBall
+        );
     }
 
     public Command deactivateIndexer() {
         return setSpeed(IndexerConstants.InactiveSpeed);
-    }
-
-    @Override
-
-    public void setRequest2(ControlRequest request) {
-        this.request = request;
-    }
-
-    public Command checkBalsl() {
-    return runOnce(() -> {hasBall? = laser.get(); /* whatever laser detection is */});
-    }
-
-    public Command 
-
-
+    };
 }
--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -4,7 +4,8 @@ public class IndexerConstants {
 
     public static final int MOTOR_ID = 0;
     public static final int LASER_ID = 0;
-    public static final boolean ActiveSpeed = 0;
-    public static final boolean InactiveSpeed = 0;
+    public static final double ActiveSpeed = 0;
+    public static final double InactiveSpeed = 0;
+    public static final double LaserCan_DefaultMeasurement = 1;
  
 }
