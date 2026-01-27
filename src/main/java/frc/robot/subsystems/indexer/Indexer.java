4eff1106a4011fd9976d706b611f6e366a21bfd8
Ethan Lian <ethanlian2000@gmail.com>
2026-01-26 17:21:37 -0800

    importing changes + updating laser

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -3,10 +3,11 @@ import com.ctre.phoenix6.controls.ControlRequest;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import frc.robot.subsystems.TelemetryManager;
 import au.grapplerobotics.LaserCan;
 import au.grapplerobotics.ConfigurationFailedException;
 
@@ -16,34 +17,16 @@ public class Indexer extends SubsystemBase {
     private TalonFX motor;
     private LaserCan lc;
 
-    private double getDistanceMm() /* command to sense distance from camera */ {
-        LaserCan.Measurement measurement = lc.getMeasurement();
-        if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
-            return measurement.distance_mm;
-        } else {
-            return -1;
-        }
-    }
+    /** boolean that's modified by checkForBall() */
+    private boolean hasBall;
 
-    private boolean hasBall; /* boolean that's modified by checkForBall() */
-
-    private boolean hasBall() /* setup formatting for boolean hasBall so it can be used in activateIndexer().either */ {
-        return hasBall;
-    }
-
-    private void checkForBall() /* command that modifies hasBall */ {
-        double x = getDistanceMm();
-        if (x == IndexerConstants.LaserCan_DefaultMeasurement) {
-            boolean hasBall = false;
-        } else {
-            boolean hasBall = true;
-        }
-    }
-
-    private Indexer() /* setup, adding motor and laser */ {
+    /** setup, adding motor and laser */
+    private Indexer() {
+        super();
         motor = new TalonFX(IndexerConstants.MOTOR_ID);
-
         lc = new LaserCan(IndexerConstants.LASER_ID);
+
+        /* new laser configs */
         try {
             lc.setRangingMode(LaserCan.RangingMode.SHORT);
             lc.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
@@ -54,19 +37,23 @@ public class Indexer extends SubsystemBase {
     }
 
     @Override
-    public void periodic() /* check for balls and makes sure motor is constantly running at desired speed */ {
+    /* check for balls and makes sure motor is constantly running at desired speed */
+    public void periodic() {
         motor.setControl(request);
         checkForBall();
     }
 
-    public void setRequest(ControlRequest request) /* type conversion/abstraction    */ {
+    /** type conversion/abstraction */
+    public void setRequest(ControlRequest request) {
         this.request = request;
     }
 
+    /** sets speed (duh) */
     public  Command setSpeed(double speed) {
         return runOnce(() -> setRequest(new VelocityVoltage(speed)));
     }
     
+    /** moves motor to speed if sense ball */
     public Command activateIndexer() {
         return Commands.either (
             setSpeed(IndexerConstants.ActiveSpeed),
@@ -75,7 +62,40 @@ public class Indexer extends SubsystemBase {
         );
     }
 
+    /** turn motor down to zero */
     public Command deactivateIndexer() {
         return setSpeed(IndexerConstants.InactiveSpeed);
+    }
+
+    /** command to sense distance from camera; used to sense if bol */
+    private double getDistanceMm() {
+        LaserCan.Measurement measurement = lc.getMeasurement();
+        if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+            return measurement.distance_mm;
+        } else {
+            return -1;
+        }
+    }
+
+    /** setup formatting for boolean hasBall so it can be used in activateIndexer().either */
+    private boolean hasBall() {
+        return hasBall;
+    }
+
+    /** command that modifies hasBall, uses getDistanceMm() */
+    private void checkForBall() {
+        double x = getDistanceMm();
+        if (x == IndexerConstants.LaserCan_DefaultMeasurement) {
+            hasBall = false;
+        } else {
+            hasBall = true;
+        }
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        super.initSendable(builder);
+        builder.addBooleanProperty("Has Ball", () -> hasBall, null);
+        TelemetryManager.makeSendableTalonFX("Indexer motor", motor, builder);
     }
 }
