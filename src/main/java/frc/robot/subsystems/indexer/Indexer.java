7bc9d59cbbe95dce12a4a77b99b3562e522dc6e3
Ethan Lian <ethanlian2000@gmail.com>
2026-01-28 17:23:15 -0800

    adding getInstance method, prototype for sensing shooter ready or not

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -11,29 +11,53 @@ import frc.robot.subsystems.TelemetryManager;
 import au.grapplerobotics.LaserCan;
 import au.grapplerobotics.ConfigurationFailedException;
 
+// TODO (ethan): only activate if shooter ready
+// TODO (ethan): ask tommy setControl(request)
 public class Indexer extends SubsystemBase {
+    /** getInstance of indexer */
+    private static Indexer instance;
+    public static Indexer getInstance() {
+        if (instance == null) {
+            instance = new Indexer();
+        }
+        return instance;
+    }
     private ControlRequest request;
 
     private TalonFX motor;
-    private LaserCan lc;
+    private LaserCan lc, lcTwo;
 
     /** boolean that's modified by checkForBall() */
     private boolean hasBall;
+
+    private boolean shooterReady = false;
+    // COMMENTED OUT /** boolean that's modified by checkForBallTwo() */
+    // private boolean hasBallTwo;
+
+    /** boolean that's modified by shooter */
 
     /** setup, adding motor and laser */
     private Indexer() {
         super();
         motor = new TalonFX(IndexerConstants.MOTOR_ID);
         lc = new LaserCan(IndexerConstants.LASER_ID);
+        lcTwo = new LaserCan(IndexerConstants.LASER_ID_2);
 
         /* new laser configs */
         try {
             lc.setRangingMode(LaserCan.RangingMode.SHORT);
             lc.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
             lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
-          } catch (ConfigurationFailedException e) {
+        } catch (ConfigurationFailedException e) {
             System.out.println("Configuration failed! " + e);
-          }
+        }
+        try {
+            lcTwo.setRangingMode(LaserCan.RangingMode.SHORT);
+            lcTwo.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+            lcTwo.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+        } catch (ConfigurationFailedException e) {
+            System.out.println("Configuration failed! " + e);
+        }
     }
 
     @Override
@@ -41,6 +65,10 @@ public class Indexer extends SubsystemBase {
     public void periodic() {
         motor.setControl(request);
         checkForBall();
+        // checkForBallTwo();
+        if (shooterReady = true) {
+            activateIndexer();
+        }
     }
 
     /** type conversion/abstraction */
@@ -76,11 +104,24 @@ public class Indexer extends SubsystemBase {
             return -1;
         }
     }
+    // COMMENTED OUT /** command to sense if bol going into shooter */
+    // private double getDistanceMmTwo() {
+    //     LaserCan.Measurement measurement = lc.getMeasurement();
+    //     if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+    //         return measurement.distance_mm;
+    //     } else {
+    //         return -1;
+    //     }
+    // }
 
     /** setup formatting for boolean hasBall so it can be used in activateIndexer().either */
     private boolean hasBall() {
         return hasBall;
     }
+    // COMMENTED OUT /** setup formatting for boolean hasBall so that it can be used to tell if shooter ready */
+    // private boolean hasBallTwo() {
+    //     return hasBallTwo;
+    // }
 
     /** command that modifies hasBall, uses getDistanceMm() */
     private void checkForBall() {
@@ -91,8 +132,19 @@ public class Indexer extends SubsystemBase {
             hasBall = true;
         }
     }
-
+    // COMMENTED OUT //** command that checks if bol is boutta be shot */
+    // private void checkForBallTwo() {
+    //     double x = getDistanceMmTwo();
+    //     if (x >= IndexerConstants.LaserCan_DefaultMeasurement) {
+    //         hasBallTwo = false;
+    //     } else if (x != -1) { /* (x != -1) is checking that the camera isn't just returning an error as ball sensed */
+    //         hasBallTwo = true;
+    //     }
+    // }
+    
     @Override
+    // TODO: AdvantageKit!
+    /** ????????? */
     public void initSendable(SendableBuilder builder) {
         super.initSendable(builder);
         builder.addBooleanProperty("Has Ball", () -> hasBall, null);
--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -6,8 +6,9 @@ public class IndexerConstants {
     // TODO (Ethan): Set LaserCan_DefaultMeasurement
     public static final int MOTOR_ID = 0;
     public static final int LASER_ID = 0;
+    public static final int LASER_ID_2 = 0;
     public static final double ActiveSpeed = 0;
     public static final double InactiveSpeed = 0;
     public static final double LaserCan_DefaultMeasurement = 1;
- 
+
 }
