9907d413c31bb6e977ad0e1837bb005d1b3c9fdf
dcao6668 <dcao6668@gmail.com>
2026-01-31 13:26:30 -0800

    Revert back to ProxyCommand in sysid framework

--- src/main/java/frc/robot/ControlsMapping.java
+++ src/main/java/frc/robot/ControlsMapping.java
@@ -9,60 +9,55 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.ctre.CtreDrive.SysIdRoutineType;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ProxyCommand;
 
 public class ControlsMapping {
 	public static void mapTeleopCommand() {
 		Drive.getInstance().setDefaultCommand((Drive.getInstance().teleopCommand()));
 		// run sysID functions
-		Drive.getInstance().getCtreDrive().setSysIdRoutine(SysIdRoutineType.STEER);
+		// Drive.getInstance().getCtreDrive().setSysIdRoutine(SysIdRoutineType.STEER);
 		
-		controller.a().onTrue(Drive.getInstance().resetPoseCommand(new Pose2d()));
-		controller.leftBumper().whileTrue(Drive.getInstance().autoAlign(true));
-		controller.rightBumper().whileTrue(Drive.getInstance().autoAlign(false));
-		controller.x().whileTrue(Drive.getInstance().autopilotAlign(true));
-		controller.y().whileTrue(Drive.getInstance().autopilotAlign(false));
+		// controller.a().onTrue(Drive.getInstance().resetPoseCommand(new Pose2d()));
+		// controller.leftBumper().whileTrue(Drive.getInstance().autoAlign(true));
+		// controller.rightBumper().whileTrue(Drive.getInstance().autoAlign(false));
+		// controller.x().whileTrue(Drive.getInstance().autopilotAlign(true));
+		// controller.y().whileTrue(Drive.getInstance().autopilotAlign(false));
 	}
 
 	public static void mapSysId() {
 		// set up sysID routine type
-		controller.a().onTrue(Commands.runOnce(
-			() -> Drive.getInstance().getCtreDrive().setSysIdRoutine(SysIdRoutineType.TRANSLATION)));
-		controller.b().onTrue(Commands.runOnce(
-			() -> Drive.getInstance().getCtreDrive().setSysIdRoutine(SysIdRoutineType.ROTATION)));
-		controller.back().onTrue(Commands.runOnce(
-			() -> Drive.getInstance().getCtreDrive().setSysIdRoutine(SysIdRoutineType.STEER)));
+		controller.a().onTrue(new InstantCommand(()->Drive.getInstance().getCtreDrive().setSysIdRoutine(SysIdRoutineType.TRANSLATION)));
+		controller.b().onTrue(new InstantCommand(()->Drive.getInstance().getCtreDrive().setSysIdRoutine(SysIdRoutineType.ROTATION)));
+		controller.back().onTrue(new InstantCommand(()->Drive.getInstance().getCtreDrive().setSysIdRoutine(SysIdRoutineType.STEER)));
 		// map the sysid routine movement directions
-		controller.leftBumper().and(controller.x()).whileTrue(
-			Drive.getInstance().getCtreDrive().sysIdDynamic(Direction.kForward)
-				.finallyDo((
-					boolean interrupted) -> {
-						if (interrupted) {
-							Drive.getInstance().setSwerveRequest(new SwerveRequest.Idle());
-						}
-					}));
-		controller.leftBumper().and(controller.x()).whileTrue(
-			Drive.getInstance().getCtreDrive().sysIdDynamic(Direction.kReverse)
-				.finallyDo((
-					boolean interrupted) -> {
-						if (interrupted) {
-							Drive.getInstance().setSwerveRequest(new SwerveRequest.Idle());
-						}
-					}));
-		controller.rightBumper().and(controller.x()).whileTrue(
-			Drive.getInstance().getCtreDrive().sysIdQuasistatic(Direction.kForward)
-				.finallyDo((
-					boolean interrupted) -> {
-						if (interrupted) {
-							Drive.getInstance().setSwerveRequest(new SwerveRequest.Idle());
-						}
-					}));
-		controller.rightBumper().and(controller.y()).whileTrue(
-			Drive.getInstance().getCtreDrive().sysIdQuasistatic(Direction.kReverse)
-				.finallyDo((
-					boolean interrupted) -> {
-						if (interrupted) {
-							Drive.getInstance().setSwerveRequest(new SwerveRequest.Idle());
-						}
-					}));
+		controller.leftBumper().and(controller.x())
+			.whileTrue(
+				new ProxyCommand(
+					()->Drive.getInstance().getCtreDrive().sysIdDynamic(Direction.kForward)
+						.finallyDo(interrupted->Drive.getInstance().getCtreDrive().setControl(new SwerveRequest.Idle()))
+				)
+			);
+		controller.leftBumper().and(controller.y())
+			.whileTrue(
+				new ProxyCommand(
+					()->Drive.getInstance().getCtreDrive().sysIdDynamic(Direction.kReverse)
+						.finallyDo(interrupted->Drive.getInstance().getCtreDrive().setControl(new SwerveRequest.Idle()))
+				)
+			);
+		controller.rightBumper().and(controller.x())
+				.whileTrue(
+					new ProxyCommand(
+						()->Drive.getInstance().getCtreDrive().sysIdQuasistatic(Direction.kForward)
+							.finallyDo(interrupted->Drive.getInstance().getCtreDrive().setControl(new SwerveRequest.Idle()))
+					)
+				);
+		controller.rightBumper().and(controller.y())
+				.whileTrue(
+					new ProxyCommand(
+						()->Drive.getInstance().getCtreDrive().sysIdQuasistatic(Direction.kReverse)
+							.finallyDo(interrupted->Drive.getInstance().getCtreDrive().setControl(new SwerveRequest.Idle()))
+					)
+				);
 	}
 }
