ca9db06a71217637593c8187b9b92f0c7a1687d3
github-0-0 <tomcao36@gmail.com>
2026-02-05 22:45:45 -0800

    Game state tracking

---
+++ src/main/java/frc/robot/GameState.java
@@ -0,0 +1,81 @@
+package frc.robot;
+
+import java.util.Optional;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+public enum GameState {
+    RED,
+    BLUE,
+    BOTH;
+    
+    public static GameState wonAuto;
+
+    public static Notifier startListenerThread() {
+        Notifier listenerThread = new Notifier(GameState::update);
+        listenerThread.setName("Game message listener");
+        listenerThread.startPeriodic(1.0);
+        return listenerThread;
+    }
+
+    public static void update() {
+        var message = DriverStation.getGameSpecificMessage();
+        if (message.length() > 0) {
+            switch (message.charAt(0)) {
+                case 'B' :
+                    wonAuto = BLUE;
+                    break;
+                case 'R' :
+                    wonAuto = RED;
+                    break;
+                default :
+                    wonAuto = BOTH;
+                    break;
+            }
+        } else {
+            wonAuto = BOTH;
+        }
+    }
+
+    private GameState getOppositeState() {
+        if (this == RED) {
+            return BLUE;
+        } else if (this == BLUE) {
+            return RED;
+        } else {
+            return BOTH;
+        }
+    }
+
+    public static GameState getCurrentState() {
+        if (Timer.getFPGATimestamp() < 30) {
+            return BOTH;
+        }
+        if ((int) (Timer.getFPGATimestamp() / 15.0) % 2 == 0) {
+            return wonAuto;
+        } else {
+            return wonAuto.getOppositeState();
+        }
+    }
+
+    public static boolean isAllianceHubActive() {
+        var state = getCurrentState();
+        if (state == BOTH) {
+            return true;
+        } 
+        
+        Optional<Alliance> currentAlliance = DriverStation.getAlliance();
+        if (currentAlliance.isPresent()) {
+            if (state == RED) {
+                return currentAlliance.get() == Alliance.Red;
+            } else {
+                return currentAlliance.get() == Alliance.Blue;
+            }
+        } else {
+            return true;
+        }
+    }
+}
