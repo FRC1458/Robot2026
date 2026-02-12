12740a1023183515a53845fb9a0e88b3fbd95c8f
github-0-0 <tomcao36@gmail.com>
2026-02-11 18:39:08 -0800

    Fixed indexer

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.indexer;
+
+import static frc.robot.subsystems.indexer.IndexerConstants.*;
 import com.ctre.phoenix6.controls.ControlRequest;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.sim.TalonFXSimState;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
@@ -15,51 +18,64 @@ import au.grapplerobotics.ConfigurationFailedException;
 // TODO (ethan): ask tommy setControl(request)
 public class Indexer extends SubsystemBase {
     /** getInstance of indexer */
-    private static Indexer instance;
-    public static Indexer getInstance() {
-        if (instance == null) {
-            instance = new Indexer();
+    private static Indexer leftInstance;
+    public static Indexer getLeftInstance() {
+        if (leftInstance == null) {
+            leftInstance = new Indexer(true);
         }
-        return instance;
+        return leftInstance;
+    }
+    private static Indexer rightInstance;
+    public static Indexer getrightInstance() {
+        if (rightInstance == null) {
+            rightInstance = new Indexer(false);
+        }
+        return rightInstance;
     }
     private ControlRequest request;
 
     private TalonFX motor;
     private TalonFXSimState motorSim;
-    private LaserCan lc, lcTwo;
+    private LaserCan lc;
 
     /** boolean that's modified by checkForBall() */
     private boolean hasBall;
 
-    private boolean shooterReady = false;
+    // private boolean shooterReady = false;
     // COMMENTED OUT /** boolean that's modified by checkForBallTwo() */
     // private boolean hasBallTwo;
 
     /** boolean that's modified by shooter */
 
     /** setup, adding motor and laser */
-    private Indexer() {
+    private Indexer(boolean isLeft) {
         super();
-        motor = new TalonFX(IndexerConstants.MOTOR_ID);
+        setName("Indexer " + (isLeft ? "Left" : "Right"));
+        motor = new TalonFX(isLeft ? L_MOTOR_ID : R_MOTOR_ID);
+        motor.getConfigurator().apply(getConfig());
         motorSim = motor.getSimState();
-        lc = new LaserCan(IndexerConstants.LASER_ID);
-        lcTwo = new LaserCan(IndexerConstants.LASER_ID_2);
+        lc = new LaserCan(isLeft ? L_LASER_ID : R_LASER_ID);
+        // lcTwo = new LaserCan(LASER_ID_2);
 
         /* new laser configs */
-        try {
-            lc.setRangingMode(LaserCan.RangingMode.SHORT);
-            lc.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
-            lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
-        } catch (ConfigurationFailedException e) {
-            System.out.println("Configuration failed! " + e);
+        for (int i = 0; i < 20; i++) {
+            try {
+                lc.setRangingMode(LaserCan.RangingMode.SHORT);
+                lc.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+                lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+                break;
+            } catch (ConfigurationFailedException e) {
+                System.out.println("Configuration failed! " + e);
+            }
         }
-        try {
-            lcTwo.setRangingMode(LaserCan.RangingMode.SHORT);
-            lcTwo.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
-            lcTwo.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
-        } catch (ConfigurationFailedException e) {
-            System.out.println("Configuration failed! " + e);
-        }
+        // try {
+        //     lcTwo.setRangingMode(LaserCan.RangingMode.SHORT);
+        //     lcTwo.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+        //     lcTwo.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+        // } catch (ConfigurationFailedException e) {
+        //     System.out.println("Configuration failed! " + e);
+        // }
+        setDefaultCommand(loadIndexer());
     }
 
     @Override
@@ -69,9 +85,14 @@ public class Indexer extends SubsystemBase {
         
         checkForBall();
         // checkForBallTwo();
-        if (shooterReady = true) {
-            activateIndexer();
-        }
+        // if (shooterReady == true) {
+        //     activateIndexer();
+        // }
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        
     }
 
     /** type conversion/abstraction */
@@ -80,22 +101,26 @@ public class Indexer extends SubsystemBase {
     }
 
     /** sets speed (duh) */
-    public  Command setSpeed(double speed) {
+    public Command setSpeed(double speed) {
         return runOnce(() -> setRequest(new VelocityVoltage(speed)));
     }
     
     /** moves motor to speed if sense ball */
-    public Command activateIndexer() {
-        return Commands.either (
-            setSpeed(IndexerConstants.ActiveSpeed),
-            setSpeed(IndexerConstants.InactiveSpeed),
+    public Command loadIndexer() {
+        return Commands.either(
+            deactivateIndexer(),
+            activateIndexer(),
             this::hasBall
-        );
+        ).repeatedly();
+    }
+
+    public Command activateIndexer() {
+        return setSpeed(ROLLING_SPEED);
     }
 
     /** turn motor down to zero */
     public Command deactivateIndexer() {
-        return setSpeed(IndexerConstants.InactiveSpeed);
+        return setSpeed(0);
     }
 
     /** command to sense distance from LaserCAN; used to sense if bol */
@@ -104,18 +129,17 @@ public class Indexer extends SubsystemBase {
         if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
             return measurement.distance_mm;
         } else {
-            return -1;
+            return Double.POSITIVE_INFINITY;
         }
     }
-    // COMMENTED OUT /** command to sense if bol going into shooter */
-    // private double getDistanceMmTwo() {
-    //     LaserCan.Measurement measurement = lc.getMeasurement();
-    //     if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
-    //         return measurement.distance_mm;
-    //     } else {
-    //         return -1;
-    //     }
-    // }
+
+    /** command that modifies hasBall, uses getDistanceMm() */
+    private void checkForBall() {
+        double x = getDistanceMm();
+        if (x <= MAXIMUM_LASER_DIST) {
+            hasBall = false;
+        }
+    }
 
     /** setup formatting for boolean hasBall so it can be used in activateIndexer().either */
     private boolean hasBall() {
@@ -125,32 +149,31 @@ public class Indexer extends SubsystemBase {
     // private boolean hasBallTwo() {
     //     return hasBallTwo;
     // }
-
-    /** command that modifies hasBall, uses getDistanceMm() */
-    private void checkForBall() {
-        double x = getDistanceMm();
-        if (x >= IndexerConstants.LaserCan_DefaultMeasurement) {
-            hasBall = false;
-        } else if (x != -1) { /* (x != -1) is checking that the camera isn't just returning an error as ball sensed */
-            hasBall = true;
-        }
-    }
+    // COMMENTED OUT /** command to sense if bol going into shooter */
+    // private double getDistanceMmTwo() {
+    //     LaserCan.Measurement measurement = lc.getMeasurement();
+    //     if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+    //         return measurement.distance_mm;
+    //     } else {
+    //         return -1;
+    //     }
+    // }
     // COMMENTED OUT //** command that checks if bol is boutta be shot */
     // private void checkForBallTwo() {
     //     double x = getDistanceMmTwo();
-    //     if (x >= IndexerConstants.LaserCan_DefaultMeasurement) {
+    //     if (x >= LaserCan_DefaultMeasurement) {
     //         hasBallTwo = false;
     //     } else if (x != -1) { /* (x != -1) is checking that the camera isn't just returning an error as ball sensed */
     //         hasBallTwo = true;
     //     }
     // }
     
-    @Override
     // TODO: AdvantageKit!
     /** ????????? */
+    @Override
     public void initSendable(SendableBuilder builder) {
         super.initSendable(builder);
         builder.addBooleanProperty("Has Ball", () -> hasBall, null);
-        TelemetryManager.makeSendableTalonFX("Indexer motor", motor, builder);
+        TelemetryManager.makeSendableTalonFX("Indexer Motor", motor, builder);
     }
 }
--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -1,14 +1,38 @@
 package frc.robot.subsystems.indexer;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.VoltageConfigs;
+
 public class IndexerConstants {
 
     // TODO: Set IDs
     // TODO (Ethan): Set LaserCan_DefaultMeasurement
-    public static final int MOTOR_ID = 0;
-    public static final int LASER_ID = 0;
-    public static final int LASER_ID_2 = 0;
-    public static final double ActiveSpeed = 0;
-    public static final double InactiveSpeed = 0;
-    public static final double LaserCan_DefaultMeasurement = 1;
+    public static final int L_MOTOR_ID = 32;
+    public static final int L_LASER_ID = 36;
+    public static final int R_MOTOR_ID = 32;
+    public static final int R_LASER_ID = 36;
+    // public static final int LASER_ID_2 = 0;
+    public static final double ROLLING_SPEED = 10; // rps
+    public static final double MAXIMUM_LASER_DIST = 100;
 
+
+    public static TalonFXConfiguration getConfig() {
+        return new TalonFXConfiguration()
+            .withSlot0(new Slot0Configs()
+                .withKV(0.0)
+                .withKP(0.3)
+                .withKI(0.001)
+                .withKD(0.0)
+                .withKA(0.1)
+                .withKS(0.1)
+                .withKV(0.1)) // placeholder values
+            .withCurrentLimits(new CurrentLimitsConfigs()
+                .withStatorCurrentLimit(40)
+                .withSupplyCurrentLimit(40))
+            .withVoltage(new VoltageConfigs()
+                .withPeakForwardVoltage(12.0)
+                .withPeakReverseVoltage(-12.0));
+    }
 }
