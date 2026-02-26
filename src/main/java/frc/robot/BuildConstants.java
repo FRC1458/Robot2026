74aab5b41831805b7f70087f5720a09ecea4a60d
dcao6668 <dcao6668@gmail.com>
2026-02-25 18:52:46 -0800

    feat: mapping sysid cmds

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 162;
-  public static final String GIT_SHA = "bba633add7f6a412e9e277d2ce1cb83903b3e7b2";
-  public static final String GIT_DATE = "2026-02-25 20:48:31 EST";
+  public static final int GIT_REVISION = 163;
+  public static final String GIT_SHA = "4cef7ab6228497f5481ccd8c3f953114b793bc02";
+  public static final String GIT_DATE = "2026-02-25 21:25:09 EST";
   public static final String GIT_BRANCH = "sotm";
-  public static final String BUILD_DATE = "2026-02-25 21:22:08 EST";
-  public static final long BUILD_UNIX_TIME = 1772072528929L;
+  public static final String BUILD_DATE = "2026-02-25 21:47:17 EST";
+  public static final long BUILD_UNIX_TIME = 1772074037013L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
