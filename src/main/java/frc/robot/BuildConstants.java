4cef7ab6228497f5481ccd8c3f953114b793bc02
dcao6668 <dcao6668@gmail.com>
2026-02-25 18:25:09 -0800

    feat: sysid for drivetrain and user AdvantageKit for logging

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 161;
-  public static final String GIT_SHA = "ce09e076753a57e0f8ab453607d72e1433288e4a";
-  public static final String GIT_DATE = "2026-02-24 20:40:50 EST";
+  public static final int GIT_REVISION = 162;
+  public static final String GIT_SHA = "bba633add7f6a412e9e277d2ce1cb83903b3e7b2";
+  public static final String GIT_DATE = "2026-02-25 20:48:31 EST";
   public static final String GIT_BRANCH = "sotm";
-  public static final String BUILD_DATE = "2026-02-25 18:28:39 EST";
-  public static final long BUILD_UNIX_TIME = 1772062119568L;
+  public static final String BUILD_DATE = "2026-02-25 21:22:08 EST";
+  public static final long BUILD_UNIX_TIME = 1772072528929L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
