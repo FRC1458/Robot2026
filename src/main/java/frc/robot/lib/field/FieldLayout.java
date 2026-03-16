bca9ca0d318562c0c1833d40ae4ded1487c971ed
Dibavi <liverisdimitri@gmail.com>
2026-03-16 16:35:19 -0700

    added getHubTargetMethod

--- src/main/java/frc/robot/lib/field/FieldLayout.java
+++ src/main/java/frc/robot/lib/field/FieldLayout.java
@@ -1,5 +1,6 @@
 package frc.robot.lib.field;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,9 +13,10 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -36,60 +38,41 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class FieldLayout {
 	//TODO: this must be tuned to the specific year's field
 	public static Field2d field;
-	public static final double FIELD_LENGTH;
-	public static final double FIELD_WIDTH;
+	public static final double FIELD_LENGTH = Units.inchesToMeters(651.223);
+	public static final double FIELD_WIDTH = Units.inchesToMeters(323.277);
 
 	public static final double APRITAG_WIDTH = Units.inchesToMeters(6.50);
-	public static AprilTagFieldLayout APRILTAG_MAP;
+	public static final AprilTagFieldLayout APRILTAG_MAP;
 
-	public static final HashMap<Pose2d, Rotation2d> ENTRY_ANGLES = new HashMap<>();
-
-	public static final List<Pose2d> ALIGN_POSES_RIGHT = new ArrayList<>();
-	public static final List<Pose2d> ALIGN_POSES_LEFT = new ArrayList<>();
-
+	public static final List<Pose2d> TARGET_HUB_POSES = new ArrayList<>();
+	
 	static {
 		try {
-			APRILTAG_MAP = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2026RebuiltAndymark.m_resourceFile);
-		} catch (Exception e) {
-			DriverStation.reportError(e.getMessage(), false);
-			APRILTAG_MAP = AprilTagLayoutGenerated.getLayout();
+			APRILTAG_MAP = AprilTagFieldLayout.loadFromResource(AprilTagFields.kDefaultField.m_resourceFile);
+			field = new Field2d();
+			SmartDashboard.putData(field);
+			int[] hubIds = {9,10,25,26};
+			Transform2d moveOut = new Transform2d(
+				Units.inchesToMeters(45), 0, Rotation2d.kZero); //TODO: tune distance
+			for (int i : hubIds) {
+				var tagPose = APRILTAG_MAP.getTagPose(i).get().toPose2d();
+				TARGET_HUB_POSES.add(tagPose.transformBy(moveOut));
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
-
-		FIELD_LENGTH = APRILTAG_MAP.getFieldLength();
-		FIELD_WIDTH = APRILTAG_MAP.getFieldWidth();
-		// APRILTAG_MAP = AprilTagLayoutGenerated.getLayout();
-		field = new Field2d();
-		SmartDashboard.putData(field);
-		// int[] reefIds = {6, 7, 8, 9, 10, 11, 17, 18, 19, 20, 21, 22};
-		// Transform2d leftReefTransform = new Transform2d(
-		// 	DriveConstants.TRACK_WIDTH / 2 + Units.inchesToMeters(1.5), -Units.inchesToMeters(13.0 / 2), Rotation2d.kPi);
-		// Transform2d rightReefTransform = new Transform2d(
-		// 	DriveConstants.TRACK_WIDTH / 2 + Units.inchesToMeters(1.5), Units.inchesToMeters(13.0 / 2), Rotation2d.kPi);
-		// for (int i : reefIds) {
-		// 	var tagPose = APRILTAG_MAP.getTagPose(i).get().toPose2d();
-		// 	var right = tagPose.transformBy(rightReefTransform);
-		// 	var left = tagPose.transformBy(leftReefTransform);
-		// 	ALIGN_POSES_RIGHT.add(right);
-		// 	ALIGN_POSES_LEFT.add(left);
-		// 	ENTRY_ANGLES_RIGHT.put(right, tagPose.getRotation().plus(Rotation2d.k180deg));
-		// 	ENTRY_ANGLES_LEFT.put(left, tagPose.getRotation().plus(Rotation2d.k180deg));
-		// }
 	}
 
-	public static Pose2d getNearestPose(Pose2d pose, boolean left) {
-		return pose.nearest(left ? ALIGN_POSES_LEFT : ALIGN_POSES_RIGHT);
-	}
-
-	public static APTarget getNearestTarget(Pose2d pose) {
-		var set = ENTRY_ANGLES;
+	public static APTarget getHubTarget(Pose2d pose) {
+		var set = TARGET_HUB_POSES;
 		var out = Collections.min(
-			set.keySet(),
+			set,
 			Comparator.comparing(
 				(Pose2d other) -> pose.getTranslation().getDistance(other.getTranslation())
 			).thenComparing(
 				(Pose2d other) ->
-					Math.abs(pose.getRotation().minus(other.getRotation()).getRadians())));
-		return new APTarget(out).withEntryAngle(set.get(out));
+				Math.abs(pose.getRotation().minus(other.getRotation()).getRadians())));
+		return new APTarget(out).withEntryAngle(out.getRotation());
 	}
 
 	public static Pose2d handleAllianceFlip(Pose2d blue_pose, boolean is_red_alliance) {
